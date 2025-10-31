bc8b278b57a40c615c25bfd788ed59f8ce4f74df
ascantlebury <aascantlebury0@example.com>
2025-10-30 22:46:28 -0700

    Remove heavy build and node_modules folders from tracking

--- frontend copy/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
-import "./src/config/firebaseConfig";
-
-export default function App() {
-  return <AppNavigator />;
-}
